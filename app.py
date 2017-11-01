7181088ebfe7ed90191333dcef76b78db37d2648
Hector Rivas Gandara <keymon@gmail.com>
2017-11-01 14:12:19 +0000

    Add /temp endpoint with test
    use requests to retrieve the response from the
    weather service.

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask
+import os
+import requests
+import json
+
+
+WORLDWEATHERONLINE_WEATHER_ENDPOINT = \
+    'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+WORLDWEATHERONLINE_API_KEY = \
+    os.environ.get('WORLDWEATHERONLINE_API_KEY', 'undefined')
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def root():
     return "weather app"
+
+
+@app.route('/temp')
+def temp():
+    params = {
+        'key': WORLDWEATHERONLINE_API_KEY,
+        'q': 'London',
+        'format': 'json'
+    }
+    response = requests.get(WORLDWEATHERONLINE_WEATHER_ENDPOINT, params=params)
+    response.raise_for_status()
+    temp = response.json()['data']['current_condition'][0]['temp_C']
+
+    return json.dumps({'temp': temp})
 
 
 if __name__ == '__main__':
--- app_tests.py
+++ app_tests.py
@@ -1,5 +1,6 @@
 from app import app
 import unittest
+import json
 
 
 class MyTestClass(unittest.TestCase):
@@ -13,6 +14,13 @@ class MyTestClass(unittest.TestCase):
     def test_root(self):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
+
+    def test_temp(self):
+        result = self.app.get('/temp')
+        self.assertEqual(result.status_code, 200)
+        data = json.loads(result.data)
+        assert 'temp' in data.keys()
+        self.assertRegexpMatches(data['temp'], r'^[0-9]+$')
 
 
 if __name__ == '__main__':
